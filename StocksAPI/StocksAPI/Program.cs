7a90c1401ea2dcce579811942141050f48d15db7
Liviu Mandras-Iura <liviu.mandras-iura@realto.io>
2025-08-18 23:16:42 +0100

    fixed spa serving

--- StocksAPI/StocksAPI/Program.cs
+++ StocksAPI/StocksAPI/Program.cs
@@ -21,14 +21,7 @@ builder.Services.AddSpaStaticFiles(options => { options.RootPath = "client-app/s
 
 var app = builder.Build();
 
-
-app.UseStaticFiles();
-app.UseSpaStaticFiles();
-
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSpaStaticFiles();
-// }
+app.UseDeveloperExceptionPage();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -37,41 +30,30 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
+
 app.UseFastEndpoints(c =>
 {
     c.Serializer.Options.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
     c.Endpoints.RoutePrefix = "api";
 });
-app.UseEndpoints(_ => { });
 
-app.UseSpa(spa =>
-{
-    spa.Options.SourcePath = "client-app/stocks";
-    if (app.Environment.IsDevelopment())
+app.UseWhen(
+    context => !context.Request.Path.StartsWithSegments("/api"),
+    appBuilder =>
     {
-        spa.UseProxyToSpaDevelopmentServer("http://localhost:5173");
-    }
-    else
-    {
-       // spa.Options.DefaultPage = "/site/wwwroot/wwwroot/index.html";
-    }
-});
-//
-// app.UseWhen(
-//     context => !context.Request.Path.StartsWithSegments("/api"),
-//     appBuilder =>
-//     {
-//         appBuilder.UseSpaStaticFiles();
-//         appBuilder.UseSpa(spa =>
-//         {
-//             spa.Options.DefaultPage= "/wwwroot/wwwroot/index.html";
-//             spa.Options.SourcePath = "client-app/stocks/dist";
-//             if (app.Environment.IsDevelopment())
-//             {
-//                 spa.UseProxyToSpaDevelopmentServer("http://localhost:5173");
-//             }
-//         });
-//     });
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseSpaStaticFiles();
+        }
+        
+        appBuilder.UseSpa(spa =>
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                spa.Options.SourcePath = "client-app/";
+                spa.UseProxyToSpaDevelopmentServer("http://localhost:5173");
+            }
+        });
+    });
 
 app.Run();
